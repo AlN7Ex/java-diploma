eb44d4d0c869c1e5f9fd3306a8a8b2173ede9a3c
aln7ex <alexmisak@yandex.ru>
2022-06-05 18:26:15 +0300

    Final Project

--- src/main/java/LinksSuggester.java
+++ src/main/java/LinksSuggester.java
@@ -1,13 +1,37 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class LinksSuggester {
 
-    public LinksSuggester(File file) throws IOException, WrongLinksFormatException {
+    private Scanner scanner;
+    private List<Suggest>  suggests = new ArrayList<>();
 
+    public LinksSuggester(File file) throws IOException, WrongLinksFormatException {
+        scanner = new Scanner(file);
+    }
+
+    public void check() {
+        while (scanner.hasNext()) {
+            String next = scanner.nextLine();
+            String[] split = next.split("\\t");
+            if (split.length != 3) {
+                throw new WrongLinksFormatException("Wrong links structure");
+            }
+            else {
+                suggests.add(new Suggest(split[0], split[1], split[2]));
+            }
+        }
     }
 
     public List<Suggest> suggest(String text) {
-        return null;
+        List<Suggest> tempSuggest = new ArrayList<>();
+        for (Suggest sug : suggests) {
+            if (text.toLowerCase().contains(sug.getKeyWord().toLowerCase())) {
+                tempSuggest.add(sug);
+            }
+        }
+        return tempSuggest;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,18 +1,69 @@
+import com.itextpdf.kernel.geom.Rectangle;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.action.PdfAction;
+import com.itextpdf.kernel.pdf.annot.PdfLinkAnnotation;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+import com.itextpdf.layout.Canvas;
+import com.itextpdf.layout.element.Link;
+import com.itextpdf.layout.element.Paragraph;
+
 import java.io.File;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+
         // создаём конфиг
         LinksSuggester linksSuggester = new LinksSuggester(new File("data/config"));
+        linksSuggester.check();
 
-        // перебираем пдфки в data/pdfs
+        int count = 0;
+//        List<Suggest> linkSuggest = linksSuggester.suggest("класс");
+        var dir = new File("data/pdfs");
+        String[] list = dir.list();
 
-        // для каждой пдфки создаём новую в data/converted
+        if (list != null) {
+//            Set<String> keyWordSet = new HashSet<>();
 
-        // перебираем страницы pdf
+            // перебираем пдфки в data/pdfs
 
-        // если в странице есть неиспользованные ключевые слова, создаём новую страницу за ней
+            for (var fileIn : dir.listFiles()) {
 
-        // вставляем туда рекомендуемые ссылки из конфига
+                // для каждой пдфки создаём новую в data/converted
+
+                var doc = new PdfDocument(new PdfReader(fileIn), new PdfWriter("data/converted/"+ list[count]));
+                ++count;
+                int numberOfPages = doc.getNumberOfPages();
+
+                // перебираем страницы pdf
+//                while (doc.)
+                for (int i = 1; i <= numberOfPages; ++i) {
+                    String textFromPage = PdfTextExtractor.getTextFromPage(doc.getPage(i));
+
+                    // если в странице есть неиспользованные ключевые слова, создаём новую страницу за ней
+
+                    List<Suggest> suggest = linksSuggester.suggest(textFromPage);
+                    var newPage = doc.addNewPage(++i);
+                    numberOfPages++;
+                    var rect = new Rectangle(newPage.getPageSize()).moveRight(10).moveDown(10);
+                    Canvas canvas = new Canvas(newPage, rect);
+                    Paragraph paragraph = new Paragraph("Suggestions:\n");
+                    paragraph.setFontSize(25);
+                        // сюда вставтье логика добавления нужных ссылок
+                    for (Suggest sug : suggest) {
+                        PdfLinkAnnotation annotation = new PdfLinkAnnotation(rect);
+                        PdfAction action = PdfAction.createURI(sug.getUrl());
+                        annotation.setAction(action);
+                        Link link = new Link(sug.getTitle(), annotation);
+                        paragraph.add(link.setUnderline());
+                        paragraph.add("\n");
+                    }
+                    canvas.add(paragraph);
+                }
+                doc.close();
+            }
+        }
     }
 }
